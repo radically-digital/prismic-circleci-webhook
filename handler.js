1bd4fbe5be22171fdecb4be0bdbe5c873026b84b
Oliver Smit <olmesm@gmail.com>
2020-03-17 15:03:37 +0000

    added secret checking and corrected response

--- handler.js
+++ handler.js
@@ -4,20 +4,33 @@ const {
   DEFAULT_BRANCH,
   VCS,
   REPO_OWNER,
-  REPO_NAME
+  REPO_NAME,
+  RANDOM_SECRET
 } = process.env;
 
 module.exports.trigger = async event => {
   const { branch } = JSON.parse(event.params);
+  const { secret } = JSON.parse(event.body);
   const body = JSON.stringify({ branch: branch || DEFAULT_BRANCH });
+
+  if (`${secret}` !== `${RANDOM_SECRET}`) {
+    return { statusCode: 401, body: "I need the secret" };
+  }
 
   try {
     const response = await got.post(
       `https://circleci.com/api/v1.1/project/${VCS}/${REPO_OWNER}/${REPO_NAME}/build?circle-token=${CIRCLE_CI_TOKEN}`,
       { body, headers: { "Content-Type": "application/json" } }
     );
-    return response.body;
+
+    return {
+      body: JSON.stringify(response.body),
+      statusCode: response.status
+    };
   } catch (error) {
-    return error.response.body;
+    return {
+      body: JSON.stringify(error.response.body),
+      statusCode: error.response.status
+    };
   }
 };
